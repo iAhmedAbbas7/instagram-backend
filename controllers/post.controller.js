91af32d419d3f406ba1faadce5bbf68fa49b8d2e
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-07-01 16:06:57 +0500

    Minor Update

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -195,7 +195,7 @@ export const getRecentPosts = expressAsyncHandler(async (req, res) => {
     .select("image");
   // IF NO POSTS FOUND
   if (!posts || posts.length === 0) {
-    return res.status(404).json({ message: "No Posts Found!", success: false });
+    return res.status(200).json({ success: true, posts: [] });
   }
   // RETURNING RESPONSE
   return res.status(200).json({ success: true, posts });
