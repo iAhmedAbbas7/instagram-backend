17f95210a73de7970c4b9c4c5198c9f0bb77d1b7
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-08-08 23:44:52 +0500

    Introduced the Story View Model for Story Feature in my App

---
+++ models/storyView.model.js
@@ -0,0 +1,27 @@
+// <= IMPORTS =>
+import mongoose from "mongoose";
+
+// <= STORY MEDIA SCHEMA =>
+const storyViewSchema = new mongoose.Schema(
+  {
+    story: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Story",
+      required: true,
+    },
+    viewer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    viewedAt: { type: Date, default: Date.now },
+    slideIndex: { type: Number, default: null },
+  },
+  { timestamps: true }
+);
+
+// <= INDEX FOR STORY FOR QUICK ACTIVE STORIES LOOKUP =>
+storyViewSchema.index({ story: 1, viewer: 1 }, { unique: true });
+
+// <= EXPORTING THE STORY VIEW SCHEMA =>
+export const StoryView = mongoose.model("StoryView", storyViewSchema);
